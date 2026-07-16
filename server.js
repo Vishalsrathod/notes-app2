396971932cc6b8c787440c3aee8d11bdbf356c0f
Vishal Rathod <chrisdetective2@gmail.com>
2026-07-16 19:06:36 +0530

    server.js

--- server.js
+++ server.js
@@ -4,13 +4,13 @@ const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const noteRoutes = require('./routes/noteRoutes');
 
-// Load environment variables
+
 dotenv.config();
 
-// Initialize app
+/
 const app = express();
 
-// Connect to database
+
 connectDB();
 
 // Middleware
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
   res.status(500).json({ message: 'Something went wrong!', error: err.message });
 });
 
-// Start server
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
